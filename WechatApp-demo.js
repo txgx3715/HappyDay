e4037e0fe6dfa7248d27a018c7e742ae95ecf2e8
txgx3715 <68771516+txgx3715@users.noreply.github.com>
2020-07-30 12:37:25 +0800

    Update WechatApp-demo.js

--- WechatApp-demo.js
+++ WechatApp-demo.js
@@ -1,7 +1,28 @@
 此处demo指单独页面下文件中的.js文件
 Page({
 data:{
-
-}
+msg : "Hello World",
+    num:6666,
+    person:{
+      name:"LiHua",
+      id:330,
+      height:180,
+      weight:330
+    }
+  
+  <！--
+  简单说明：Page用于处理单独页面下的信息内容，如将你需要的数据储存在data中，msg的类型为String，num的类型为int，person的类型为object
+    ,
+    list:[
+      {
+        id:0,
+        name:"值1"
+      },{
+        id:1,
+        name:"值2"
+      },{id:3,
+        name:"值3"
+      }
+    ]
 })
 
