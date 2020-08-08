49fa534af6c09191c5b41106d952d3c62ce80c00
Mobacola <68771516+txgx3715@users.noreply.github.com>
2020-08-08 23:35:13 +0800

    Add files via upload

---
+++ Three kinds of data.py
@@ -0,0 +1,13 @@
+character_name = "Tom"
+character_age = "55"
+print("There have a man was " + character_name + " ")
+print("he was " + character_age + " years old")
+
+is_Male = True
+is_Female = False
+
+character_name = "Mike"
+character_age = "45"
+
+print("he really like this name " + character_name + " ")
+print("but he don't like being " + character_age + " ")
